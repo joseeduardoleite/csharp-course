d3637b1ba305c007ceaafe9df147a266c125461b
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-10-17 20:45:05 -0300

    exMatrizes

---
+++ Matrizes/Matriz.ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Matriz.ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[,] matriz;
+           System.Console.Write("Tamanho da Matriz: ");
+           int numeroMatriz = int.Parse(Console.ReadLine());
+           matriz = new int[numeroMatriz , numeroMatriz];
+
+           for (int i = 0; i < numeroMatriz; i++)
+           {
+               string[] values = Console.ReadLine().Split(' ');
+
+               for (int j = 0; j < numeroMatriz; j++)
+               {
+                   matriz[i,j] = int.Parse(values[j]);
+               }
+               
+           }
+
+           System.Console.WriteLine("Diagonal principal:");
+           for (int i = 0; i < numeroMatriz; i++)
+           {
+               System.Console.WriteLine(matriz[i,i] + " ");
+           }
+           System.Console.WriteLine("------------");
+
+           int contador = 0;
+           for (int i = 0; i < numeroMatriz; i++)
+           {
+               for (int j = 0; j < numeroMatriz; j++)
+               {
+                   if (matriz[i,j] < 0)
+                    contador++;
+               }
+           }
+           System.Console.WriteLine("Numeros negativos: " + contador);
+        }
+    }
+}
---
+++ Matrizes/Matriz.ex2/Matriz.ex2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Matriz.ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ler dois numeros inteiros M e N, ler uma mat de M l por N c contendo numeros inteiros,
+            // podendo haver repeticoes. Ler um numero int X que pertence a matriz. Para cada ocorrencia de X,
+            // mostrar os valores a esquerda, acima, a direita e abaixo de X, quando houver, conforme exemplo.
+
+            string[] linha = Console.ReadLine().Split(' ');
+            int m = int.Parse(linha[0]);
+            int n = int.Parse(linha[1]);
+
+            int[,] matriz = new int[m,n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            System.Console.WriteLine("Insira o numero que deseja: ");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+               for (int j = 0; j < n; j++)
+               {
+                   if (matriz[i,j] == numero)
+                   {
+                       System.Console.WriteLine("Posicao " + i + "," + j + ":");
+                       if (j > 0) {
+                           System.Console.WriteLine("Esquerda: " + matriz[i,j - 1]);
+                       }
+                       if (i > 0) {
+                           System.Console.WriteLine("Acima: " + matriz[i - 1, j]);
+                       }
+                       if (j < n - 1) {
+                           System.Console.WriteLine("Direita: " + matriz[i, j + 1]);
+                       }
+                       if (i < m -1) {
+                           System.Console.WriteLine("Abaixo: " + matriz[i + 1, j]);
+                       }
+                   }
+               }
+            }
+        }
+    }
+}
