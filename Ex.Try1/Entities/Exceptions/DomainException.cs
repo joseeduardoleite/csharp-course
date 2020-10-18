d06803c3207a303fa2c79eb82634f11c71d47594
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-10-18 15:04:47 -0300

    exTryCatch1

---
+++ Ex.Try1/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ Ex.Try1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Entities;
+using Entities.Exceptions;
+
+namespace Ex.Try1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+            
+    }
+}
+
