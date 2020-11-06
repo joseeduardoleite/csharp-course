5d8d093d31d0532bbe4cd9c38eeb002ade523992
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-11-05 22:17:08 -0300

    Generics

---
+++ GenericProblema2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Entities;
+using Services;
+
+namespace GenericProblema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            System.Console.WriteLine("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            System.Console.WriteLine("Max: ");
+            System.Console.WriteLine(max);
+        }
+    }
+}
