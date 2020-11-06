5f72ba4e6b6b0daa770afa8c0c66042513b0c994
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-11-05 22:19:29 -0300

    ServicesSolution

---
+++ ISolution/Services/RentalService.cs
@@ -0,0 +1,39 @@
+using System;
+using Entities;
+
+namespace Services
+{
+    public class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private ITaxService _taxService;
+
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);  
+            }
+
+            double tax = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
