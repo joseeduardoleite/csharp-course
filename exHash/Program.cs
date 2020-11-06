245298e5b672378ce59f1c497d91137d7a398fd6
José Eduardo <57038911+eduardomalikoski@users.noreply.github.com>
2020-11-05 22:13:03 -0300

    Hash

---
+++ exHash/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Entities;
+
+namespace exHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.WriteLine("Enter file full path: ");
+            //Nome da arquivo .txt por ex.
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    System.Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ exHashSet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int nmrStudent = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nmrStudent; i++)
+            {
+                int codetudent = int.Parse(Console.ReadLine());
+                courseA.Add(codetudent);
+            }
+
+            Console.Write("How many students for course B? ");
+            nmrStudent = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nmrStudent; i++)
+            {
+                int codeStudent = int.Parse(Console.ReadLine());
+                courseB.Add(codeStudent);
+            }
+
+            Console.Write("How many students for course C? ");
+            nmrStudent = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nmrStudent; i++)
+            {
+                int codeStudent = int.Parse(Console.ReadLine());
+                courseC.Add(codeStudent);
+            }
+
+            HashSet<int> allStudent = new HashSet<int>();
+            allStudent.UnionWith(courseB);
+            allStudent.UnionWith(courseC);
+            Console.WriteLine("Total students: " + allStudent.Count);
+        }
+    }
+}
